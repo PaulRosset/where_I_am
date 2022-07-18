8f463d7d5ce33d728c7b17cfe3d98fe0c93a0eea
Paul Rosset <paul.rosset@qonto.com>
2022-07-18 09:39:20 +0200

    v1.0.0

---
+++ README.md
@@ -0,0 +1,5 @@
+# Where I am ?
+
+A simple swift app to geolocalize the user, to know where he is situated.
+
+Handy, when in big city to give friends the address of where the user is located.
--- where_I_am/ViewController.swift
+++ where_I_am/ViewController.swift
@@ -6,14 +6,68 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var InfosView: UIStackView!
+    @IBOutlet weak var LocationDescription: UILabel!
+
+    let locationManager = CLLocationManager()
+    var lat: Double = 0.0
+    var long: Double = 0.0
+    var strLocation = ""
+    
+    let pasterboard = UIPasteboard.general
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        InfosView.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didInfoView))
+        InfosView.addGestureRecognizer(tap)
+
+        // Request access to location data
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let newLat = location.coordinate.latitude
+            let newlong = location.coordinate.longitude
+            if (newLat != lat || newlong != long) {
+                // call func to retrieve new coordinates geocoded.
+                let geoCode = CLGeocoder()
+                let location = CLLocation(latitude: CLLocationDegrees(newLat), longitude: CLLocationDegrees(newlong))
+                self.lat = newLat
+                self.long = newlong
+                geoCode.reverseGeocodeLocation(location) { clPlacemark, error in
+                    if error != nil {
+                        self.LocationDescription.text = error?.localizedDescription.description
+                        return;
+                    }
+                    if let sl = clPlacemark?.last {
+                        self.strLocation = "\(sl.subThoroughfare ?? "") \(sl.thoroughfare ?? ""), \(sl.postalCode ?? ""), \(sl.locality ?? ""), \(sl.country ?? "")"
+                        self.LocationDescription.text = self.strLocation
+                    }
+                }
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        self.LocationDescription.text = error.localizedDescription.description
+    }
+    
+    @objc func didInfoView(sender: UITapGestureRecognizer) {
+        pasterboard.string = self.strLocation
+        self.LocationDescription.text = "Copied!"
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            self.LocationDescription.text = self.strLocation
+        }
+        
+    }
 }
 
